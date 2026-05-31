9031a1510e3d8ff24e801779409000d8200158af
Ananya Pagidimarri <ananaya.pagidimarri@gmail.com>
2026-05-31 10:32:02 +0530

    update

--- backend/cache/redisClient.js
+++ backend/cache/redisClient.js
@@ -4,24 +4,27 @@
  */
 
 const Redis = require('ioredis');
-const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = require('../config/envConfig');
+const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, REDIS_URL } = require('../config/envConfig');
 const logger = require('../utils/logger');
 
 // Connection options
-const redisOptions = {
-  host: REDIS_HOST || '127.0.0.1',
-  port: REDIS_PORT || 6379,
-  retryStrategy: (times) => {
-    // Reconnect after
-    return Math.min(times * 50, 2000);
+const getRedisOptions = () => {
+  if (REDIS_URL) {
+    // Note: Upstash requires tls object if using rediss://
+    return REDIS_URL;
   }
+  const options = {
+    host: REDIS_HOST || '127.0.0.1',
+    port: REDIS_PORT || 6379,
+    retryStrategy: (times) => Math.min(times * 50, 2000),
+  };
+  if (REDIS_PASSWORD) {
+    options.password = REDIS_PASSWORD;
+  }
+  return options;
 };
 
-if (REDIS_PASSWORD) {
-  redisOptions.password = REDIS_PASSWORD;
-}
-
-const redisClient = new Redis(redisOptions);
+const redisClient = new Redis(getRedisOptions());
 
 redisClient.on('connect', () => {
   logger.info('[Redis] Connected to distributed cache.');
